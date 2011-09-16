bb5748f59ea0648308a15dd0e35f02c418bcb05f
Wesley Ellis <tahnok@gmail.com>
2011-09-15 21:41:17 -0400

    initial commit. Dependent on licensing of original script

---
+++ README.md
@@ -0,0 +1,18 @@
+Author: Wesley Ellis (@tahnok on twitter)
+Contact: wesleyjellis@gmail.com
+
+You need
+
+* curl
+* grep
+* sed
+* scrot
+* zenity
+
+Add run chmod +x on the script and you are ready to go!
+
+If you're in Ubuntu you can bind any key you want to run the script
+
+## License ##
+
+This code is released under GPLv3. See the file named copying for the full license
---
+++ screenshot.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+screenshot='screenshot';
+nano=`date '+%d-%m-%y-%N'`;
+extension='.png';
+file="$HOME/Desktop/$screenshot-$nano$extension";
+
+sleep 3; scrot -s -b -q 0 $file;
+
+curl -d username=YOURUSERNAME -d password=YOURPASSWORD -c imgur_cookie.txt http://api.imgur.com/2/signin;
+
+TEXT=$(curl -F "image"=@"$file" -F "key"="9d67c345536acff4b53e81d0b11d8c59" --cookie imgur_cookie.txt http://api.imgur.com/2/account/images | grep -Eo '<[a-z_]+>http[^<]+'|sed 's/^<.\|_./\U&/g;s/_/ /;s/<\(.*\)>/\1:\ /');
+
+zenity --info --title="Imgur Upload" --text="$TEXT";
+
+exit 0
